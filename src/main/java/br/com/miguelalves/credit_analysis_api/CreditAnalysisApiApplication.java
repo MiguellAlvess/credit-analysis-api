54f871e2a6667fe7105f2c3b0695b98f97b467c5
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-10 10:43:56 -0300

    chore: add inital project setup

---
+++ src/main/java/br/com/miguelalves/credit_analysis_api/CreditAnalysisApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.credit_analysis_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAnalysisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAnalysisApiApplication.class, args);
+	}
+
+}

df521420c27ce623003366d763c4a0da5a86c947
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-11 11:14:45 -0300

    fix: adjust the ID type in the CompanyRepository to UUID

--- src/main/java/br/com/miguelalves/credit_analysis_api/company/repository/CompanyRepository.java
+++ src/main/java/br/com/miguelalves/credit_analysis_api/company/repository/CompanyRepository.java
@@ -1,12 +1,13 @@
 package br.com.miguelalves.credit_analysis_api.company.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.miguelalves.credit_analysis_api.company.domain.Company;
 
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
 
     Optional<Company> findByCnpj(String cnpj);
 }

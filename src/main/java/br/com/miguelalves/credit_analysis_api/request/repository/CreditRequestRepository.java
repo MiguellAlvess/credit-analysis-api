5658bb92fb96c617bfcdaf769f19cbfef4529b5d
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-11 10:44:18 -0300

    fix: adjust the ID type in the CreditRequestRepository to UUID

--- src/main/java/br/com/miguelalves/credit_analysis_api/request/repository/CreditRequestRepository.java
+++ src/main/java/br/com/miguelalves/credit_analysis_api/request/repository/CreditRequestRepository.java
@@ -1,9 +1,11 @@
 package br.com.miguelalves.credit_analysis_api.request.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.miguelalves.credit_analysis_api.request.domain.CreditRequest;
 
-public interface CreditRequestRepository extends JpaRepository<CreditRequest, Long> {
+public interface CreditRequestRepository extends JpaRepository<CreditRequest, UUID> {
 
 }

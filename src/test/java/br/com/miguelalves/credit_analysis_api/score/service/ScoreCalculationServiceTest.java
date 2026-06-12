a0c05807e0ddd37c970d083e0b39b32563d77c84
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-12 11:13:46 -0300

    test: refactor score calculation method parameters to simplify

--- src/test/java/br/com/miguelalves/credit_analysis_api/score/service/ScoreCalculationServiceTest.java
+++ src/test/java/br/com/miguelalves/credit_analysis_api/score/service/ScoreCalculationServiceTest.java
@@ -17,65 +17,65 @@ import br.com.miguelalves.credit_analysis_api.score.domain.RiskLevel;
 @ExtendWith(MockitoExtension.class)
 public class ScoreCalculationServiceTest {
 
-    @InjectMocks
-    private ScoreCalculationService scoreCalculationService;
+        @InjectMocks
+        private ScoreCalculationService scoreCalculationService;
 
-    @Test
-    void shouldCalculateScoreForActiveCompanyWithMoreThanFiveYearsAndHighRevenue() {
-        Company company = Company.create(
-                "12345678000195",
-                "Empresa XPTO",
-                RegistrationStatus.ACTIVE,
-                "58400000",
-                "Campina Grande",
-                "PB",
-                LocalDate.now().minusYears(6));
-        CreditRequest creditRequest = CreditRequest.create(
-                company,
-                new BigDecimal("100000.00"),
-                new BigDecimal("1500000.00"));
+        @Test
+        void shouldCalculateScoreForActiveCompanyWithMoreThanFiveYearsAndHighRevenue() {
+                Company company = Company.create(
+                                "12345678000195",
+                                "Empresa XPTO",
+                                RegistrationStatus.ACTIVE,
+                                "58400000",
+                                "Campina Grande",
+                                "PB",
+                                LocalDate.now().minusYears(6));
+                CreditRequest creditRequest = CreditRequest.create(
+                                company,
+                                new BigDecimal("100000.00"),
+                                new BigDecimal("1500000.00"));
 
-        int score = scoreCalculationService.calculate(company, creditRequest);
+                int score = scoreCalculationService.calculate(company, creditRequest.getAnnualRevenue());
 
-        assertThat(score).isEqualTo(1000);
-    }
+                assertThat(score).isEqualTo(1000);
+        }
 
-    @Test
-    void shouldCalculateScoreForInactiveCompanyWithLessThanFiveYearsAndLowRevenue() {
-        Company company = Company.create(
-                "12345678000195",
-                "Empresa XPTO",
-                RegistrationStatus.CLOSED,
-                "58400000",
-                "Campina Grande",
-                "PB",
-                LocalDate.now().minusYears(2));
-        CreditRequest creditRequest = CreditRequest.create(
-                company,
-                new BigDecimal("100000.00"),
-                new BigDecimal("500000.00"));
+        @Test
+        void shouldCalculateScoreForInactiveCompanyWithLessThanFiveYearsAndLowRevenue() {
+                Company company = Company.create(
+                                "12345678000195",
+                                "Empresa XPTO",
+                                RegistrationStatus.CLOSED,
+                                "58400000",
+                                "Campina Grande",
+                                "PB",
+                                LocalDate.now().minusYears(2));
+                CreditRequest creditRequest = CreditRequest.create(
+                                company,
+                                new BigDecimal("100000.00"),
+                                new BigDecimal("500000.00"));
 
-        int score = scoreCalculationService.calculate(company, creditRequest);
+                int score = scoreCalculationService.calculate(company, creditRequest.getAnnualRevenue());
 
-        assertThat(score).isEqualTo(0);
-    }
+                assertThat(score).isEqualTo(0);
+        }
 
-    @Test
-    void shouldClassifyHighRiskWhenScoreIsBetweenZeroAnd399() {
-        assertThat(scoreCalculationService.classifyRiskLevel(399))
-                .isEqualTo(RiskLevel.HIGH);
-    }
+        @Test
+        void shouldClassifyHighRiskWhenScoreIsBetweenZeroAnd399() {
+                assertThat(scoreCalculationService.classifyRiskLevel(399))
+                                .isEqualTo(RiskLevel.HIGH);
+        }
 
-    @Test
-    void shouldClassifyMediumRiskWhenScoreIsBetween400And699() {
-        assertThat(scoreCalculationService.classifyRiskLevel(400))
-                .isEqualTo(RiskLevel.MEDIUM);
-    }
+        @Test
+        void shouldClassifyMediumRiskWhenScoreIsBetween400And699() {
+                assertThat(scoreCalculationService.classifyRiskLevel(400))
+                                .isEqualTo(RiskLevel.MEDIUM);
+        }
 
-    @Test
-    void shouldClassifyLowRiskWhenScoreIsBetween700And1000() {
-        assertThat(scoreCalculationService.classifyRiskLevel(700))
-                .isEqualTo(RiskLevel.LOW);
-    }
+        @Test
+        void shouldClassifyLowRiskWhenScoreIsBetween700And1000() {
+                assertThat(scoreCalculationService.classifyRiskLevel(700))
+                                .isEqualTo(RiskLevel.LOW);
+        }
 
 }

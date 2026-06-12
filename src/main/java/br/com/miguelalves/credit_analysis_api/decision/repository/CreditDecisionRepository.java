35f4fc1bf94f58f59ad7f70bdbd8a937c6ef7b78
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-06-12 11:46:45 -0300

    fix: adjust credit decision repository to use uuid

--- src/main/java/br/com/miguelalves/credit_analysis_api/decision/repository/CreditDecisionRepository.java
+++ src/main/java/br/com/miguelalves/credit_analysis_api/decision/repository/CreditDecisionRepository.java
@@ -1,8 +1,13 @@
 package br.com.miguelalves.credit_analysis_api.decision.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.miguelalves.credit_analysis_api.decision.domain.CreditDecision;
 
-public interface CreditDecisionRepository extends JpaRepository<CreditDecision, Long> {
+public interface CreditDecisionRepository extends JpaRepository<CreditDecision, UUID> {
+
+    Optional<CreditDecision> findByCreditRequestId(UUID creditRequestId);
 }
